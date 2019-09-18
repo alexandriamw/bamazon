9c01825cf1cba8e199180c13b2bb7ba84502b6c8
alexandriamw <alexandriawagner84@gmail.com>
2019-09-17 22:52:24 -0500

    working on buystuff function, added prompt for user to enter item ID and quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,13 +19,33 @@ function afterConnection() {
     connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
         if (err) throw err;
         console.table(res);
+        buyStuff();
     });
 }
 
 function buyStuff() {
-    inquirer
-        .prompt({
+    inquirer.prompt([
+        {
+            name: "itemID",
+            type: "input",
+            message: "Please enter the item_id of the item you would like to buy"
+        },
 
-        })
-        .then
+        {
+            name: "quantity",
+            type: "input",
+            message: "How many would you like to buy?"
+        }
+    ]).then
+
 }
+
+
+
+
+
+// 7. Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request. If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
+
+// 8. However, if your store _does_ have enough of the product, you should fulfill the customer's order.
+//    * This means updating the SQL database to reflect the remaining quantity.
+//    * Once the update goes through, show the customer the total cost of their purchase.
